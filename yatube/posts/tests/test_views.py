5ee3af2e2e3d755d62b9767214badc06d2fbc7a2
Dmitry Tsybulenko <45.vilka@mail.ru>
2022-12-08 20:02:12 +0300

    GO

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -84,68 +84,6 @@ class PostPagesTests(TestCase):
         self.assertEqual(group_title_test_0, 'Aga')
 
 
-"""class PaginatorViewsTest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.author = User.objects.create_user(username='Author5')
-        cls.group = Group.objects.create(
-            title='Test group',
-            slug='Test_slug',
-            description='Description'
-        )
-        for i in range(1, 14):
-            cls.post = Post.objects.create(
-                text='Text_post',
-                author=cls.author,
-                group=cls.group
-            )
-        cls.author = Client()
-        cls.author.force_login(cls.author)
-
-    def setUp(self):
-        self.guest_client = Client()
-        self.user = User.objects.create_user(username='Author6')
-        self.authorized_client = Client()
-        self.authorized_client.force_login(self.user)
-
-    def test_first_page_contains_ten_records(self):
-        Paginator_obj = {
-            reverse('posts:index'): 10,
-            reverse('posts:group_posts', kwargs={'slug': self.group.slug}): 10,
-            reverse('posts:profile', kwargs={'username': self.author}): 10,
-        }
-        for page, number in Paginator_obj.items():
-            with self.subTest(page=page):
-                response = self.guest_client.get(page)
-                self.assertEqual(len(response.context['page_obj']), number)
-
-    def test_second_page_contains_three_records(self):
-        Paginator_obj = {
-            reverse('posts:index'): 3,
-            reverse('posts:group_posts', kwargs={'slug': self.group.slug}): 3,
-            reverse('posts:profile', kwargs={'username': self.author}): 3,
-        }
-        for page, number in Paginator_obj.items():
-            with self.subTest(page=page):
-                response = self.guest_client.get(page + '?page=2')
-                self.assertEqual(len(response.context['page_obj']), number) 
-                ААААААААААААААААААААААААААААААААААААААААААААААААААА
-                типо черта от какого то говна, главное не забыт ьудалить
-
-    def test_post_list_page_show_correct_context(self):
-        Шаблон group_post сформирован с правильным контекстом.
-        response = self.authorized_client.get(reverse('posts:group_list'))
-        # Взяли первый элемент из списка и проверили, что его содержание
-        # совпадает с ожидаемым
-        first_object = response.context['object_list'][0]
-        task_title_0 = first_object.title
-        task_description_0 = first_object.text
-        task_slug_0 = first_object.slug
-        self.assertEqual(task_title_0, 'Заголовок')
-        self.assertEqual(task_description_0, 'Текст')
-        self.assertEqual(task_slug_0, 'test-slug')"""
-
 
 
 
